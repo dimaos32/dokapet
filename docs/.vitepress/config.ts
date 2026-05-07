e553759bc60a0af98ad06d7849eb3f1e1c7997cd
Dmitrii Osadchenko <osadchenko.d@gmail.com>
2026-05-08 01:01:09 +0300

    Настраивает автодеплой при push в master

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/dokapet/',
   title: 'Dokapet',
   description: 'Pet project for doc systems',
   themeConfig: {
