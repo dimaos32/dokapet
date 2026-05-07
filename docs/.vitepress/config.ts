ebd2156c119470d0ebc41495a19700b636c0aad5
Dmitrii Osadchenko <osadchenko.d@gmail.com>
2026-05-08 00:21:46 +0300

    Добавляет фэйковые страницы в документацию, задает структуру и конфиг

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  title: 'Dokapet',
+  description: 'Pet project for doc systems',
+  themeConfig: {
+    nav: [
+      { text: 'Обзор', link: '/' },
+      { text: 'Напитки', link: '/drinks/' },
+      { text: 'Еда', link: '/food/' },
+    ],
+
+    sidebar: [
+      { text: 'Обзор', link: '/' },
+      {
+        text: 'Напитки',
+        link: '/drinks/',
+        collapsed: true,
+        items: [{ text: 'Масала', link: '/drinks/masala' }],
+      },
+      {
+        text: 'Еда',
+        link: '/food/',
+        collapsed: true,
+        items: [
+          {
+            text: 'Салаты',
+            collapsed: true,
+            items: [
+              { text: 'Первый салат', link: '/food/salads/salad-1' },
+              { text: 'Второй салат', link: '/food/salads/salad-2' },
+            ],
+          },
+          {
+            text: 'Основные блюда',
+            collapsed: true,
+            items: [
+              { text: 'Первое горячее', link: '/food/mains/main-1' },
+              { text: 'Второе горячее', link: '/food/mains/main-2' },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: "es5",
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  proseWrap: "preserve",
+};
